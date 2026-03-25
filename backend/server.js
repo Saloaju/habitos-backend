ce1fda8711ba1ca4fc4e16f3297b6c5ceeaa0417
Salo Ajú <13002911@galileo.edu>
2026-03-25 16:47:21 -0600

    Configurar Backend para Vercel

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://127.0.0.1:27017/habitosDB')
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ Conectado a MongoDB'))
   .catch(err => console.error('Error:', err));
 
